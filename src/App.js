22b7395384f4d1a28e4453222b8ed615f4f056f8
taymaz ranjbari <taymazranjbari@gmail.com>
2022-08-21 08:59:32 +0430

    class 3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import Header from "./components/Header";
-import Slideshow from './components/Slideshow';
-import Card from './components/Card';
-import Footer from './components/Footer';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Home from "./components/Home";
+import Welcome from "./components/Welcome";
+import Event from "./components/Event";
+import Login from "./components/Login";
+import CarLIst from "./components/CarLIst";
+// import Header from "./components/Header";
+// import Slideshow from './components/Slideshow';
+// import Card from './components/Card';
+// import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <Header />      
+      {/* <Header />      
       <Slideshow />
       <Card />
-      <Footer />
-   
+      <Footer /> */}
+      {/* <Home color={"Red"} back={"black"} />
+      <Welcome name={"sara"} />
+
+      <Event /> */}
+      {/* <Login level={"tttt"} /> */}
+      <CarLIst />
     </>
   );
 }
