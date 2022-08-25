b73480e1f79b347287f1b704be804cc70a323bfd
taymaz ranjbari <taymazranjbari@gmail.com>
2022-08-25 19:55:56 +0430

    update  form  App Add

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Welcome from "./components/Welcome";
 import Event from "./components/Event";
 import Login from "./components/Login";
 import CarLIst from "./components/CarLIst";
+import ComA from "./components/TestCss/ComA";
+import ComB from "./components/TestCss/ComB";
 // import Header from "./components/Header";
 // import Slideshow from './components/Slideshow';
 // import Card from './components/Card';
@@ -23,7 +25,11 @@ function App() {
 
       <Event /> */}
       {/* <Login level={"tttt"} /> */}
-      <CarLIst />
+      {/* <CarLIst />
+       */}
+
+      {/* <ComA />
+      <ComB /> */}
     </>
   );
 }
